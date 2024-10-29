846a4175cc540ea9271819ec64b0d849857ce916
kretuuuu <kretu.new@gmail.com>
2024-10-29 20:35:53 +0100

    +++

--- js/main.js
+++ js/main.js
@@ -1,36 +1,57 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"); 
+const mn = document.querySelector(".main");
+let planet_list = new Array();
+let shows;
 
 function setAttributes(el, attrs) {
-  for(var key in attrs) {
+  for (var key in attrs) {
     el.setAttribute(key, attrs[key]);
-
   }
 }
 
-const planet_list = new Array();
-
-const planets = function () {
-  fetch("http://localhost/api_planets/")
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res[0].name);
-
-      res.forEach((element) => {
-        const planImg = document.createElement("img");
-        setAttributes(planImg, {"src": `http://localhost/api_planets/img/${element.name}.png`, "alt": element.name});
-        planImg.classList.add("planety");
-        planet_list.push(planImg)
-        // planImg.addEventListener("click", powieksz);
-        container.appendChild(planImg);
-        planImg.addEventListener("click", function powieksz(){
-          this.classList.toggle("powiekszenie");
-
-        });
-      });
-    })
-    .catch((error) => console.error("[ERROR] ", error));
+const onTop = function() {
+  console.log(this.getBoundingClientRect())
+  this.classList.toggle("on-top");
+  container.classList.toggle("on-top");
+  mn.classList.toggle("on-top");
+  shows = this;
+  planet_list.forEach(element => {
+    if(element.planetElement!=shows)
+    {
+      // console.error(element)
+      element.planetElement.classList.toggle("hidden");
+    }
+  });
 };
 
 
 
-planets();
+const main = () => {
+  fetch("http://localhost/api_planets/")
+    .then((res) => res.json())
+    .then((res) => {
+      planet_list = res;
+      res.forEach((element) => {
+        const planImg = document.createElement("img");
+        setAttributes(planImg, {
+          src: `http://localhost/api_planets/img/${element.name}.png`,
+          alt: element.name,
+          role: "button",
+        });
+        planImg.classList.add("planets");
+        element.planetElement = planImg;
+        container.appendChild(planImg);
+        planImg.addEventListener("click", onTop);
+      });
+      console.log(res);
+    })
+    .catch((error) => console.error("[ERROR] ", error));
+};
+
+// window.onclick = function close(event) {
+//   if (event.target != shows) {
+//     shows.classList.remove("on-top");
+//   }
+// };
+
+main();
